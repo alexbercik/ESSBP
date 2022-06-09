ee6b35455615850f34e9fd55782125453649bcd8
Alex Bercik <alex.bercik@mail.utoronto.ca>
2022-06-09 12:15:05 -0400

    make burgers work

--- Driver/RunBurgers.py
+++ Driver/RunBurgers.py
@@ -32,33 +32,42 @@ obj_name = None
 
 # Time marching
 tm_method = 'rk4' # explicit_euler, rk4
-dt = 0.0001
+dt = 0.001
+# note: should set according to courant number C = a dt / dx
 dt_init = dt
 t_init = 0
-tf = 2
+tf = 10
 
 # Domain
-xmin = -1
+xmin = 0
 xmax = 1
-isperiodic = True
+bc = 'periodic'
 
 # Spatial discretization
-disc_type = 'lgl' 
-nn = 60
+disc_type = 'div' # 'div', 'had', 'dg'
+disc_nodes = 'lgl' # 'lg', 'lgl', 'nc', 'csbp', 'dg', 'fd'
+p = 3
 nelem = 10 # optional, number of elements
 nen = 0 # optional, number of nodes per element
-p = 3
+surf_type = 'symmetric'
+had_flux = 'ec' # 2-point numerical flux used in hadamard form
+diss_type = None
 use_split_form = True
-sat_flux_type = 'ec'
+split_alpha = 1/2
 
 # Initial solution
-q0 = None
-q0_type = 'GassnerSinWave_coarse' # 'GassnerSinWave', '..._cont', '..._coarse' 'GaussWave', 'SinWave'
+q0 = None # can overwrite q0_type from DiffEq
+q0_type = 'GassnerSinWave' # 'GassnerSinWave', '..._cont', '..._coarse' 'GaussWave', 'SinWave'
 
 # Other
 bool_plot_sol = False
+bool_plot_exa = True
 print_sol_norm = False
-cons_obj_name = ('Energy','Conservation') # 'Energy', 'Conservation', 'None'
+
+obj_name = None
+cons_obj_name = ('Energy','Conservation','Entropy') # 'Energy', 'Conservation', 'None'
+settings = {}
+
 
 ''' Set diffeq and solve '''
 
@@ -69,23 +78,25 @@ elif disc_type == 'dg':
 else:
     c_solver = PdeSolverSbp
 
-diffeq = Burgers(para, obj_name, q0_type, use_split_form)
+diffeq = Burgers(para, obj_name, q0_type, use_split_form, split_alpha)
 diffeq.plt_style_exa_sol = {'color':'r','linestyle':'-','marker':'','linewidth':2}
-savefile = 'burgers_alpha_23'
-title=r'Burgers Eqn, $\alpha=2/3$, EC flux'
+savefile = 'burgers_alpha1_symSAT'
+title=r'Burgers Eqn, $\alpha=1$, sym flux'
 
-solver = c_solver(diffeq,                              # Diffeq
+
+solver = c_solver(diffeq, settings,                     # Diffeq
                   tm_method, dt, tf,                    # Time marching
                   q0,                                   # Initial solution
-                  p, disc_type, nn,                     # Discretization
-                  nelem, nen, sat_flux_type,
-                  isperiodic, xmin, xmax,               # Domain
+                  p, disc_type,             # Discretization
+                  surf_type, diss_type, had_flux,
+                  nelem, nen, disc_nodes,
+                  bc, xmin, xmax,         # Domain
                   obj_name, cons_obj_name,              # Other
                   bool_plot_sol, print_sol_norm)
 
 #solver.weakform = True
-#solver.force_steady_solution()
-#solver.perturb_q0()
+solver.force_steady_solution()
+solver.perturb_q0()
 A = solver.check_eigs(plt_save_name=savefile+'_eigs',returnA=True,title='Eigenvalues: ' + title,exact_dfdq=True)
 eigs = np.linalg.eigvals(A)
 max_eig = max(eigs.real)
@@ -94,11 +105,11 @@ def theory_fn(time):
 
 solver.solve()
 diffeq.plt_style_sol[0] = {'color':'b','linestyle':'-','marker':'','linewidth':3}
-#solver.plot_sol(plt_save_name=savefile+'_sol',title=title)
-#solver.plot_error(method='max diff',savefile=savefile+'_error', extra_fn=theory_fn, extra_label='Theory', title=title)
+solver.plot_sol(plt_save_name=savefile+'_sol',title=title)
+solver.plot_error(method='max diff',savefile=savefile+'_error', extra_fn=theory_fn, extra_label='Theory', title=title)
 #from Methods.Analysis import animate
 #animate(solver, plotargs={'display_time':True},skipsteps=100)
 
-solver.solve()
+#solver.solve()
 solver.plot_sol()
 solver.plot_cons_obj()
--- Source/DiffEq/Burgers.py
+++ Source/DiffEq/Burgers.py
@@ -10,6 +10,7 @@ import numpy as np
 
 from Source.DiffEq.DiffEqBase import PdeBaseCons
 import Source.Methods.Functions as fn
+from numba import njit
 
 class Burgers(PdeBaseCons):
     '''
@@ -27,25 +28,28 @@ class Burgers(PdeBaseCons):
     pde_order = 1
 
     def __init__(self, para=None, obj_name=None, q0_type='SinWave',
-                 use_split_form=False):
+                 use_split_form=False, split_alpha=2/3):
 
         super().__init__(para, obj_name, q0_type)
         self.use_split_form = use_split_form
+        self.split_alpha = split_alpha
 
-    def calcE(self, q):
+    def calcEx(self, q):
 
         E = 0.5*q**2
         return E
 
-    def dEdx(self, q):
+    def dExdx(self, q):
 
         if self.use_split_form:
             #q_diag = fn.diag(q)
             #dEdx = (1/3)*fn.gm_gv((fn.lm_gm(self.der1,q_diag) + fn.gm_lm(q_diag,self.der1)), q)
-            dEdx = (1/3)*((self.der1 @ q**2) + (q * (self.der1 @ q)))
+            #dEdx = (1/3)*((self.der1 @ q**2) + (q * (self.der1 @ q)))
+            dEdx = (self.split_alpha/2)*fn.gm_gv(self.Dx, q**2) + (1-self.split_alpha)*(q * fn.gm_gv(self.Dx, q))
         else:
-            E = self.calcE(q)
-            dEdx = self.der1 @ E
+            E = self.calcEx(q)
+            dEdx = fn.gm_gv(self.Dx, E)
+            #dEdx = self.Dx @ E
 
         return dEdx
 
@@ -65,10 +69,11 @@ class Burgers(PdeBaseCons):
         if self.use_split_form:
             # these both do the same, but the second is a bit faster
             #dfdq = -(1/3)*(2*fn.lm_gm(self.der1,fn.diag(q)) + fn.diag(self.der1@q) + fn.gm_lm(fn.diag(q),self.der1))
-            dfdq = -(1/3)*(2*np.multiply(self.der1[:,:,None],q) + fn.diag(self.der1 @ q) + fn.gm_lm(fn.diag(q),self.der1))
+            #dfdq = -(1/3)*(2*np.multiply(self.der1[:,:,None],q) + fn.diag(self.der1 @ q) + fn.gm_lm(fn.diag(q),self.der1))
+            dfdq = -self.split_alpha*fn.gm_gv_colmultiply(self.Dx,q) - (1-self.split_alpha)*(fn.diag(fn.gm_gv(self.Dx, q)) + fn.gdiag_gm(q,self.Dx))
         else:
             # this does the same as the base function, just a bit faster
-            dfdq = - np.multiply(self.der1[:,:,None],q)
+            dfdq = - fn.gm_gv_colmultiply(self.Dx,q)
             
         return dfdq
 
@@ -84,4 +89,21 @@ class Burgers(PdeBaseCons):
 
         dEdq_eig_abs = abs(dEdq)
         return dEdq_eig_abs
+    
+    def entropy(self,q):
+        e = q**2/2
+        return e
+    
+    @njit
+    def ec_Ex(qL,qR):
+        ''' entropy conservative 2-point flux for the Hadamard form '''
+        fx = (qL**2 + qL*qR + qR**2)/6
+        return fx
+    
+    @njit   
+    def central_fix_Ex(qL,qR):
+        ''' a central 2-point flux for hadamard form.
+        This allows us to jit the hadamard flux functions. '''
+        f = fn.arith_mean(qL**2/2,qR**2/2)
+        return f
 
