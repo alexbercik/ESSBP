8698e52a804c8a1c6c9667c909df1a425c94b63c
Alex Bercik <alex.bercik@mail.utoronto.ca>
2024-11-03 21:41:53 -0500

    temp: change euler driver file for testing on niagara

--- Driver/Run2dEuler.py
+++ Driver/Run2dEuler.py
@@ -30,8 +30,10 @@ test_case = 'vortex' # density_wave, vortex
 
 # Time marching
 tm_method = 'rk4' # 'explicit_euler', 'rk4'
-dt = 0.001
-tf = 0.500 #nts * dt # set to None to do automatically or use a convergence criterion, or 'steady'
+dt = 0.005
+# for vortex, maxeig < 1.6, so CFL = 0.1 gives dt = 0.16 * dx and CFL = 0.01 gives dt = 0.016 * dx
+tf = 4.0 #nts * dt # set to None to do automatically or use a convergence criterion, or 'steady'
+# for vortex, one period is t=20
 check_resid_conv = False
 
 # Domain
@@ -40,14 +42,16 @@ xmax = (5.,5.)
 bc = 'periodic' # 'periodic', 'dirichlet'
 
 # Spatial discretization
-disc_type = 'had' # 'div', 'had'
+disc_type = 'div' # 'div', 'had'
 disc_nodes = 'csbp' # 'lg', 'lgl', 'nc', 'csbp', 'dg', 'fd'
 p = 4
 nelem = (1,1) # number of elements
 nen = 30 # optional, number of nodes per element
-surf_diss = {'diss_type':'nd', 'jac_type':'scalar', 'coeff':1., 'average':'simple', 'entropy_fix':True}
+surf_diss = {'diss_type':'nd', 'jac_type':'matmat', 'coeff':1., 'average':'simple', 
+             'entropy_fix':True, 'P_derigs':True, 'A_derigs':True, 'maxeig':'rusanov'}
 had_flux = 'ranocha' # 2-point numerical flux used in hadamard form
-vol_diss = {'diss_type':'nd', 'jac_type':'scalar', 's':'p+1', 'coeff':0.005, 'fluxvec':'sw', 'bdy_fix':True, 'use_H':False}
+vol_diss = {'diss_type':'nd', 'jac_type':'matmat', 's':'p+1', 'coeff':3.125/5**(p+1),
+            'fluxvec':'dt', 'bdy_fix':True, 'use_H':True, 'entropy_fix':True, 'avg_half_nodes':True}
 
 # Initial solution
 q0 = None
@@ -57,10 +61,10 @@ q0_type = 'exact'
 bool_plot_sol = False
 print_sol_norm = False
 print_residual = False
-cons_obj_name=('Energy','Entropy') # note: should I modify this for systems?
+cons_obj_name=('Energy','Entropy','Conservation') # note: should I modify this for systems?
 settings = {'metric_method':'exact',
             'use_optz_metrics':False} # extra things like for metrics
-skip_ts = 0
+skip_ts = 99
 
 
 ''' Set diffeq and solve '''
@@ -78,12 +82,17 @@ solver = PdeSolverSbp(diffeq, settings,
                   bc, xmin, xmax,     
                   cons_obj_name,      
                   bool_plot_sol, print_sol_norm,
-                  print_residual, check_resid_conv)
-solver.skip_ts = skip_ts
+                  print_residual, check_resid_conv,
+                  sparse=True,sat_sparse=True)
+#solver.skip_ts = skip_ts
 
 #eigs = solver.check_eigs(returneigs=True)
 #solver.solve()
+#print('Error: ', solver.calc_error())
 #solver.plot_sol()#var2plot_name='entropy')
 #solver.plot_cons_obj()
-#print('Error: ', solver.calc_error())
 #solver.plot_sol()
+#solver.check_conservation()
+
+q0 = diffeq.set_q0()
+dqdt = solver.dqdt(q0,0.)
